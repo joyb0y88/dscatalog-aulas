02de47ad07fc030aae2d71f93c2105889ff50a56
Brunno Carvalho <brunno.ddcbjj@gmail.com>
2024-01-12 16:53:42 -0300

    Basic beans validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -1,8 +1,5 @@
 package com.devsuperior.dscatalog.dto;
 
-import com.devsuperior.dscatalog.entities.Category;
-import com.devsuperior.dscatalog.entities.Product;
-
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
@@ -10,15 +7,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    
+    @Size(min = 5, max = 60, message = "Deve ter entre 5 e 60 caracteres")
+    @NotBlank(message = "Campo Requerido")
     private String name;
+    
+    @NotBlank(message = "Campo Requerido")
     private String description;
+    
+    @Positive(message = "Preço deve ser um valor positivo")
     private Double price;
     private String imgUrl;
+    
+    @PastOrPresent(message = "Preço deve ser um valor positivo")
     private Instant date;
 
     private List<CategoryDTO> categories = new ArrayList<>();
--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -7,13 +7,20 @@ import java.util.Set;
 
 import com.devsuperior.dscatalog.entities.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class UserDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Campo Obrigatório")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Favor entrar um email válido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
